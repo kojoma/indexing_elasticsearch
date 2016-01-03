5a389caacb78ff890574791245a3d1afd5c16c76
kojoma <masha319@hotmail.co.jp>
2016-01-04 00:26:24 +0900

    wip

---
+++ test.rb
@@ -0,0 +1,38 @@
+require 'nokogiri'
+
+file = "/Users/masha/Music/iTunes/iTunes Music Library.xml"
+xml  = Nokogiri::XML(open(file).read)
+
+songs = xml.xpath('plist/dict/dict/dict')
+
+song_info_list = []
+for song in songs
+    song_info = {}
+    key = ""
+
+keys = song.xpath('key')
+keys.each do |key|
+    p key.content
+end
+    song.each do |element|
+      if element.tag == "key"
+            key = element.text
+        else
+            song_info[key] = element.text
+            song_info_list.push(song_info)
+        end
+    end
+
+if 0
+    for element in song
+        if element.tag == "key"
+            key = element.text
+        else
+            song_info[key] = element.text
+            song_info_list.push(song_info)
+        end
+    end
+end
+end
+
+p song_info_list[0]
