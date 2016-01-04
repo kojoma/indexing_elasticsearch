35a3cb5485dac05fbb7f967b39bf3f89d267a867
kojoma <masha319@hotmail.co.jp>
2016-01-05 00:06:55 +0900

    elasticsearchにデータ登録するためのjsonファイルを出力

--- test.rb
+++ test.rb
@@ -1,9 +1,6 @@
 require 'nokogiri'
 
-file = ARGV[0]
-xml  = Nokogiri::XML(open(file).read)
-
-songs = xml.xpath('plist/dict/dict/dict')
+# xmlファイルから取得する曲情報のkey
 key_names = [
   'Name',
   'Artist',
@@ -22,17 +19,32 @@ key_names = [
   'Album Rating'
 ]
 
+# コマンドライン引数で指定されたxmlファイルを読み込み
+file = ARGV[0]
+xml  = Nokogiri::XML(open(file).read)
+
+# 曲情報を取得して整形
+songs = xml.xpath('plist/dict/dict/dict')
 song_info_list = []
-for song in songs
-    song_info = {}
+songs.each do |song|
+  song_info = {}
 
-    keys = song.xpath('key')
-    keys.each do |key|
-        if key_names.include?(key.content)
-            key_name = key.content
-            song_info[key_name] = key.next.content
-        end
+  keys = song.xpath('key')
+  keys.each do |key|
+    if key_names.include?(key.content)
+      key_name = key.content
+      song_info[key_name] = key.next.content
     end
+  end
 
-    song_info_list.push(song_info)
+  song_info_list.push(song_info)
+end
+
+# elasticsearchに登録ためのjsonファイルに書き出し
+File.open('itunes.json', 'w') do |file|
+  song_info_list.each do |song_info|
+    file.puts('{')
+    song_info.each { |name, value| file.puts "\"#{name}\": \"#{value}\"," }
+    file.puts('},')
+  end
 end
